da688efd68f47a0e1c7565fccf8a51d52beca9f3
Kandrw <doofydak@gmail.com>
2024-10-12 20:39:40 +0700

    Подготовил mlp к вызову из бека

---
+++ Back-hakaton/MLP_detect/mlp/test.py
@@ -0,0 +1,14 @@
+from utils.data_loader import get_data_loaders
+from utils.train_helper import save_model
+import json
+# from models.classification import ClassificationModel
+# from models.detection import DetectionModel
+
+
+# from . import models
+
+# from . import models
+
+from models import *
+
+models.classification.ClassificationModel
--- test_mlp.py
+++ test_mlp.py
@@ -2,7 +2,27 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'mlp')))
 
-from mlp.api_mlp import Train
+# from mlp.api_mlp import Train
 
 
-Train()
+# Train()
+
+
+# import os
+# from models.classification import ClassificationModel
+# from models.detection import DetectionModel
+# from utils.data_loader import get_data_loaders
+# from utils.train_helper import save_model
+# import json
+
+from utils.data_loader import get_data_loaders
+from utils.train_helper import save_model
+import json
+# from models.classification import ClassificationModel
+# from models.detection import DetectionModel
+
+
+from . import models
+
+
+models.classification.ClassificationModel
